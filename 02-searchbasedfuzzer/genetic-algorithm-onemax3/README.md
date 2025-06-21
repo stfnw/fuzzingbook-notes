77e06ab31bd0d2df5a9455c90328c27fddc9960a
Stefan Walter <stefan_walter@posteo.de>
2025-06-21 14:20:04 +0200

    https://www.fuzzingbook.org/html/SearchBasedFuzzer.html

---
+++ 02-searchbasedfuzzer/genetic-algorithm-onemax3/README.md
@@ -0,0 +1,17 @@
+Like previous version, but with different selection mechanism.
+This one performs very bad. Example output:
+
+```
+$ cargo run
+...
+Generation  991: Best Fitness = 128
+Generation  992: Best Fitness = 126
+Generation  993: Best Fitness = 118
+Generation  994: Best Fitness = 124
+Generation  995: Best Fitness = 116
+Generation  996: Best Fitness = 134
+Generation  997: Best Fitness = 121
+Generation  998: Best Fitness = 133
+Generation  999: Best Fitness = 133
+Best Individual: 10101011110101110111101000111011110001110110100111011111001111001111011101001111111010001100000011010111111111001110111011111101111001111011010011001101110111101101111100100111010101111111111110110001
+```
---
+++ 02-searchbasedfuzzer/genetic-algorithm-onemax3/src/main.rs
@@ -0,0 +1,146 @@
+// SPDX-FileCopyrightText: 2025 stfnw
+// SPDX-License-Identifier: MIT
+
+mod rng;
+
+fn main() {
+    let mut rng = rng::Rng::seeded(42);
+    let individual = genetic_algorithm(&mut rng);
+    println!("Best Individual: {}", individual);
+}
+
+/// Run the genetic algorithm and return the best evolved individual.
+fn genetic_algorithm(rng: &mut rng::Rng) -> Individual {
+    /* Constants for the algorithm. ******************************************/
+    let population_size: usize = 100;
+    let genome_size: usize = 200;
+    let mutation_rate: f64 = 0.01;
+
+    // Here we can for example run either a fixed number of generations, or
+    // until the fitness value hits a maximum value that is "good enough".
+    let generations: usize = 1000;
+    // In this case the maximum possible fitness is the genome size.
+    let good_enough_fitness: usize = genome_size;
+    /*************************************************************************/
+
+    // Generate new population of random individuals.
+    let mut population = Population::new(rng, population_size, genome_size);
+
+    for generation in 0..generations {
+        // Selection: fitter individuals are chosen with higher probability.
+        let fitnesses: Vec<_> = population
+            .0
+            .iter()
+            .map(|ind| ind.fitness() as f64)
+            .collect();
+
+        // Select some percentage of the current population without modification.
+        let mut new_population = Vec::with_capacity(population_size);
+        let percentage = 5;
+        for _ in 0..(population_size / percentage) {
+            new_population.push(select(rng, &population, &fitnesses));
+        }
+
+        // Fill the rest of the new generations population through genetic
+        // operations.
+        while new_population.len() < population_size {
+            // Crossover.
+            let parent1 = select(rng, &population, &fitnesses);
+            let parent2 = select(rng, &population, &fitnesses);
+            let mut child = crossover(rng, &parent1, &parent2);
+
+            // Mutation.
+            mutate(rng, &mut child, mutation_rate);
+
+            new_population.push(child);
+        }
+
+        // Replace population with next generation / new population.
+        population = Population(new_population);
+
+        // Print status.
+        let best_fitness = population.0[0].fitness();
+        println!(
+            "Generation {:4}: Best Fitness = {}",
+            generation, best_fitness
+        );
+
+        if best_fitness >= good_enough_fitness {
+            break;
+        }
+    }
+
+    // Return best individual.
+    population.0[0].clone()
+}
+
+struct Population(Vec<Individual>);
+
+impl Population {
+    fn new(rng: &mut rng::Rng, population_size: usize, genome_size: usize) -> Self {
+        Self(
+            (0..population_size)
+                .map(|_| Individual::new(rng, genome_size))
+                .collect(),
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Individual {
+    genome: Vec<bool>,
+}
+
+impl std::fmt::Display for Individual {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        for &bit in self.genome.iter() {
+            write!(f, "{}", bit as usize)?;
+        }
+        Ok(())
+    }
+}
+
+impl Individual {
+    fn new(rng: &mut rng::Rng, genome_size: usize) -> Self {
+        let genome = (0..genome_size).map(|_| rng.bool()).collect();
+        Individual { genome }
+    }
+
+    fn fitness(&self) -> usize {
+        self.genome.iter().filter(|&&gene| gene).count()
+    }
+}
+
+/// Select random individuals from a population weighted by fitness.
+fn select(rng: &mut rng::Rng, population: &Population, fitnesses: &[f64]) -> Individual {
+    rng.choice_w(&population.0, fitnesses).clone()
+}
+
+/// One-point crossover between individual vectors.
+fn crossover(rng: &mut rng::Rng, parent1: &Individual, parent2: &Individual) -> Individual {
+    assert!(parent1.genome.len() == parent2.genome.len());
+    let genome_size = parent1.genome.len();
+    let point = rng.int(genome_size as u64) as usize;
+
+    let mut child_chromosome = Vec::with_capacity(genome_size);
+    for i in 0..genome_size {
+        if i < point {
+            child_chromosome.push(parent1.genome[i]);
+        } else {
+            child_chromosome.push(parent2.genome[i]);
+        }
+    }
+
+    Individual {
+        genome: child_chromosome,
+    }
+}
+
+/// Randomly flip a bit according to the mutation rate.
+fn mutate(rng: &mut rng::Rng, individual: &mut Individual, mutation_rate: f64) {
+    for gene in &mut individual.genome {
+        if rng.f64() < mutation_rate {
+            *gene = !*gene; // Flip the gene
+        }
+    }
+}
